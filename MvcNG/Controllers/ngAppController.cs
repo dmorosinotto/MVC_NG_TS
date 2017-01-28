ce96b31ad42f1ff6046bbf983e4a31751fd0074c
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-01-28 01:57:08 +0100

    Add logic to inject NAME in ViewBag based of Identity

--- MvcNG/Controllers/ngAppController.cs
+++ MvcNG/Controllers/ngAppController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web
 using System.Web.Mvc;
 
 namespace MvcNG.Controllers
@@ -11,7 +11,12 @@ namespace MvcNG.Controllers
         //
         // GET: /ngApp/main
         public ActionResult main() { //DON'T USE Index - DON'T WORK!
-            ViewBag.WAIT = 5000;            
+            ViewBag.WAIT = 5000;
+            if (User.Identity.IsAuthenticated) {
+               ViewBag.NAME = User.Identity.Name;
+            } else {
+               ViewBag.NAME = "Pippo"; 
+            }
             return View();            
         }
 
